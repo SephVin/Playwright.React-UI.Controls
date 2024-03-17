54e426ce4a8e16092c4c180df0d18e9a2f146490
SephVin <vinseph236@gmail.com>
2024-03-17 19:31:28 +0500

    Remove obsolete

--- RunStorybook.ps1
+++ RunStorybook.ps1
@@ -1,12 +1,10 @@
-$registry = 'https://nexus.kontur.host/repository/kontur-npm-group/'
-
 $version = "0.0.0"
 $version = npm --version
 $major, $minor, $patch = $version.Split('.')
 
 if (($major -lt 8) -or (($major -eq 8) -and ($minor -lt 19))) {
   'installing new npm version'
-  npm -g --registry $registry install npm@8
+  npm -g install npm@8
 }
 
 'npm version is ok'
