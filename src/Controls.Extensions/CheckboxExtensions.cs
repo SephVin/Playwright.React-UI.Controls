437e09ca2a90e91ee6528b45ae898bb73fbb2ee8
SephVin <vinseph236@gmail.com>
2024-03-17 19:31:27 +0500

    Add CheckboxExtensions options

--- src/Controls.Extensions/CheckboxExtensions.cs
+++ src/Controls.Extensions/CheckboxExtensions.cs
@@ -1,21 +1,30 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Microsoft.Playwright;
 
 namespace Playwright.ReactUI.Controls.Extensions;
 
 public static class CheckboxExtensions
 {
-    public static async Task WaitCheckedAsync(this Checkbox checkbox)
-        => await checkbox.Expect().ToBeCheckedAsync().ConfigureAwait(false);
+    public static async Task WaitCheckedAsync(
+        this Checkbox checkbox,
+        LocatorAssertionsToBeCheckedOptions? options = default)
+        => await checkbox.Expect().ToBeCheckedAsync(options).ConfigureAwait(false);
 
-    public static async Task WaitUncheckedAsync(this Checkbox checkbox)
-        => await checkbox.Expect().Not.ToBeCheckedAsync().ConfigureAwait(false);
+    public static async Task WaitUncheckedAsync(
+        this Checkbox checkbox,
+        LocatorAssertionsToBeCheckedOptions? options = default)
+        => await checkbox.Expect().Not.ToBeCheckedAsync(options).ConfigureAwait(false);
 
-    public static async Task WaitEnabledAsync(this Checkbox checkbox)
-        => await checkbox.Expect().ToBeEnabledAsync().ConfigureAwait(false);
+    public static async Task WaitEnabledAsync(
+        this Checkbox checkbox,
+        LocatorAssertionsToBeEnabledOptions? options = default)
+        => await checkbox.Expect().ToBeEnabledAsync(options).ConfigureAwait(false);
 
-    public static async Task WaitDisabledAsync(this Checkbox checkbox)
-        => await checkbox.Expect().ToBeDisabledAsync().ConfigureAwait(false);
+    public static async Task WaitDisabledAsync(
+        this Checkbox checkbox,
+        LocatorAssertionsToBeDisabledOptions? options = default)
+        => await checkbox.Expect().ToBeDisabledAsync(options).ConfigureAwait(false);
 
     public static async Task CheckAsync(this Checkbox checkbox)
     {
