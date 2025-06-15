b94083b58f7ec08f6efb225627fbea422c5a8f96
SephVin <vinseph236@gmail.com>
2025-06-15 18:01:09 +0500

    Тесты на SelectExtensions

--- src/Controls.Extensions/PortalExtensions.cs
+++ src/Controls.Extensions/PortalExtensions.cs
@@ -1,30 +1,17 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.Playwright;
 
 namespace Playwright.ReactUI.Controls.Extensions;
 
 public static class PortalExtensions
 {
-    [Obsolete("Use WaitToBeVisibleAsync")]
-    public static async Task WaitPresenceAsync(
-        this Portal portal,
-        LocatorAssertionsToHaveCountOptions? options = default)
-        => await portal.WaitToBeVisibleAsync(options).ConfigureAwait(false);
-
-    [Obsolete("Use WaitToBeHiddenAsync")]
-    public static async Task WaitAbsenceAsync(
-        this Portal portal,
-        LocatorAssertionsToHaveCountOptions? options = default)
-        => await portal.WaitToBeHiddenAsync(options).ConfigureAwait(false);
-
     public static async Task WaitToBeVisibleAsync(
         this Portal portal,
         LocatorAssertionsToHaveCountOptions? options = default)
-        => await portal.Expect().ToHaveCountAsync(1, options).ConfigureAwait(false);
+        => await portal.ExpectV2().ToBeVisibleAsync(options).ConfigureAwait(false);
 
     public static async Task WaitToBeHiddenAsync(
         this Portal portal,
         LocatorAssertionsToHaveCountOptions? options = default)
-        => await portal.Expect().ToHaveCountAsync(0, options).ConfigureAwait(false);
+        => await portal.ExpectV2().ToBeHiddenAsync(options).ConfigureAwait(false);
 }
